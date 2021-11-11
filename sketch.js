40e64e50f797f3d7036b88ac195054f79d1f4fb6
Samanga Narayanan <74295785+samanga-n@users.noreply.github.com>
2021-11-11 14:13:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var Balloon, database;
+var position;
+
+
+function setup(){
+  database=firebase.database()
+
+  console.log(database);
+  createCanvas(500,500);
+
+  Balloon = createSprite(250,250,10,10);
+  Balloon.shapeColor = "red";
+  var bPos=database.ref("balloon/position")
+  bPos.on("value",readPosition,showError) 
+
+
+
+}
+
+function draw(){
+  background("white");
+  
+    if(keyDown(LEFT_ARROW)){
+      writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+      writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+      writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+      writePosition(0,+1);
+    }
+    drawSprites();
+   
+}
+
+function writePosition(x,y){
+  database.ref('balloon/position').set({
+    'x':position.x+x,
+    'y':position.y+y
+    })
+    
+ 
+}
+
+function readPosition(data){
+ position=data.val()
+ Balloon.x=position.x
+ Balloon.y=position.y
+}
+
+function showError(){
+  console.log("error in writing procedure")
+}
